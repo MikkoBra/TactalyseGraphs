2eff33b499602027485693ba02b875de10092870
MikkoBra <m.brandon.1@student.rug.nl>
2023-05-27 14:44:26 +0200

    LineProcessor tests

--- graph_app/data/preprocessors/line_processor.py
+++ graph_app/data/preprocessors/line_processor.py
@@ -4,6 +4,12 @@ from ..excel_reader import ExcelReader
 
 
 class LineProcessor(Preprocessor):
+    """
+    Class that handles data extraction and processing specific to line graphs. It allows for extracting the required
+    stats to be graphed from an excel file, and for generating a map containing the parameters needed by the line graph
+    module.
+    """
+
     def __init__(self):
         self.__randomizer = Randomizer()
         self.__reader = ExcelReader()
@@ -13,7 +19,7 @@ class LineProcessor(Preprocessor):
         Function that provides a list of headers to use for graphing the line plots.
 
         :param player_pos: Abbreviated position of the player whose stats to graph.
-        :return: DataFrame containing the required stats to graph.
+        :return: Pandas Series containing the required stats to graph.
         """
         stats_file = "graph_app/files/Stats per position.xlsx"
         stats_pd = ExcelReader().read_file(stats_file)
@@ -23,10 +29,18 @@ class LineProcessor(Preprocessor):
 
     def extract_line_data(self, param_map):
         """
-        Function that extracts all required data from the passed player match data Excel file.
+        Function that takes the name of the player from the passed parameter map, and generates a line plot.
 
-        :param param_map:
-        :return: DataFrame containing the player's match data (player_df), columns to use for graphing (columns).
+        :param param_map: Parameter map containing information that should be used to create the graph. The player's
+        name (player) is required. Optional parameters are the name of the player to compare to (compare), the
+        stat to graph (stat), the player's league (league), and tactalyse's start and end dates (start_date, end_date).
+        If compare is omitted, the graph will be a single-player graph. If stat is omitted, a random stat is selected.
+        The rest is fully optional.
+        :return: Parameter map with the player's name (player), a DataFrame with the player's data (player_data), the
+        compare player's name (compare) and data (compare_data), the passed league if any (league), the player's
+        position as the abbreviation from the player file (main_pos), in full (player_pos), and abbreviated
+        (main_pos_short), tactalyse start and end dates (start_date, end_date), the stat to graph (stat), and columns
+        to use for graphing (columns).
         """
         line_map = {'type': "line"}
 
@@ -40,6 +54,13 @@ class LineProcessor(Preprocessor):
         return line_map
 
     def set_player(self, param_map, line_map):
+        """
+        Function that sets the player's name in the line graph parameter map. If it was not passed, it is randomized.
+
+        :param param_map: Parameter map containing data passed to the API endpoint.
+        :param line_map: Parameter map to be used by the line graph module.
+        :return: Line graph parameter map updated with a player name (player).
+        """
         player = param_map.get('player')
         if not player:
             player = self.__randomizer.random_player()
@@ -47,6 +68,15 @@ class LineProcessor(Preprocessor):
         return line_map
 
     def set_compare(self, param_map, line_map):
+        """
+        Function that sets the compare player's name in the line graph parameter map, along with their match data.
+        If no name was included in the input parameter map, it is entirely omitted.
+
+        :param param_map: Parameter map containing data passed to the API endpoint.
+        :param line_map: Parameter map to be used by the line graph module.
+        :return: Line graph parameter map updated with a compare player name (compare), and their match data in a
+        DataFrame (compare_data).
+        """
         if not param_map.get('compare'):
             return line_map
 
@@ -58,6 +88,14 @@ class LineProcessor(Preprocessor):
         return line_map
 
     def set_league(self, param_map, line_map):
+        """
+        Function that sets the player's league in the line graph parameter map. If no league name was included in the
+        input parameter map, it is set to "League".
+
+        :param param_map: Parameter map containing data passed to the API endpoint.
+        :param line_map: Parameter map to be used by the line graph module.
+        :return: Line graph parameter map updated with the league's name (league).
+        """
         league = param_map.get('league')
         if league:
             league = league.upper()
@@ -67,6 +105,16 @@ class LineProcessor(Preprocessor):
         return line_map
 
     def set_player_data(self, line_map):
+        """
+        Function that sets the player's match data in the line graph parameter map, along with their position in
+        different formats.
+
+        :param param_map: Parameter map containing data passed to the API endpoint.
+        :param line_map: Parameter map to be used by the line graph module.
+        :return: Line graph parameter map updated with a player match DataFrame (player_data), the player's position
+        as stated in the match data file (main_pos), the corresponding general position name (player_pos), and the
+        abbreviation of the general position (main_pos_short).
+        """
         player = line_map.get('player')
         player_df = self.__reader.player_data(player)
         line_map.update({'player_data': player_df})
@@ -82,6 +130,15 @@ class LineProcessor(Preprocessor):
         return line_map
 
     def set_tactalyse_data(self, param_map, line_map):
+        """
+        Function that sets the start and end dates for tactalyse's services for the passed player.
+        If no dates were included in the input parameter map, they're set to None.
+
+        :param param_map: Parameter map containing data passed to the API endpoint.
+        :param line_map: Parameter map to be used by the line graph module.
+        :return: Line graph parameter map updated with a start date for tactalyse's services (start_date), and an end
+        date (end_date).
+        """
         start_date = param_map.get('start_date')
         line_map.update({'start_date': start_date})
 
@@ -90,9 +147,25 @@ class LineProcessor(Preprocessor):
         return line_map
 
     def set_stats(self, param_map, line_map):
+        """
+        Function that sets the stat to be graphed in the line graph parameter map.
+        If no stat was included in the input parameter map, it is randomized.
+
+        :param param_map: Parameter map containing data passed to the API endpoint.
+        :param line_map: Parameter map to be used by the line graph module.
+        :return: Line graph parameter map updated with a stat to be graphed (stat).
+        """
         stat = param_map.get('stat')
         if not stat:
             stat = self.__randomizer.random_player_stat()
         columns = [stat]
         line_map.update({'columns': columns})
         return line_map
+
+    @property
+    def randomizer(self):
+        return self.__randomizer
+
+    @property
+    def reader(self):
+        return self.__reader
--- tests/data/preprocessors/line_processor_tests.py
+++ tests/data/preprocessors/line_processor_tests.py
@@ -7,3 +7,99 @@ class TestLineProcessor(unittest.TestCase):
 
     def setUp(self):
         self.processor = LineProcessor()
+        self.line_map = {"key": "value"}
+        self.mock_return_value = MagicMock(return_value="mock")
+
+    def test_set_player(self):
+        params = {"player": "name"}
+        result = self.processor.set_player(params, self.line_map)
+        expected = {"key": "value", "player": "name"}
+        self.assertEqual(expected, result)
+
+    def test_set_player_random(self):
+        with patch.object(self.processor.randomizer, 'random_player', new=self.mock_return_value) as mock_random:
+            result = self.processor.set_player({}, {})
+            mock_random.assert_called_once()
+            self.assertTrue("player" in result.keys())
+
+    def test_set_compare_empty(self):
+        result = self.processor.set_compare({}, self.line_map)
+        self.assertEqual(self.line_map, result)
+
+    def test_set_compare(self):
+        params = {"compare": "name"}
+        with patch.object(self.processor.reader, 'player_data', new=self.mock_return_value) as mock_read:
+            result = self.processor.set_compare(params, self.line_map)
+            mock_read.assert_called_once_with("name")
+            expected = {"key": "value", "compare": "name", "compare_data": "mock"}
+            self.assertEqual(expected, result)
+
+    def test_set_league(self):
+        params = {"league": "LEA"}
+        result = self.processor.set_league(params, self.line_map)
+        expected = {"key": "value", "league": "LEA"}
+        self.assertEqual(expected, result)
+
+    def test_set_league_lowercase_input(self):
+        params = {"league": "lea"}
+        result = self.processor.set_league(params, self.line_map)
+        expected = {"key": "value", "league": "LEA"}
+        self.assertEqual(expected, result)
+
+    def test_set_league_no_league(self):
+        result = self.processor.set_league({}, self.line_map)
+        expected = {"key": "value", "league": "League"}
+        self.assertEqual(expected, result)
+
+    def test_set_player_data(self):
+        params = {"player": "name"}
+        mock_file_pos = MagicMock(return_value="pos_f")
+        mock_pos = MagicMock(return_value={"pos_f": "pos_l"})
+        mock_short_pos = MagicMock(return_value={"pos_f": "pos_s"})
+        with patch.object(self.processor.reader, 'player_data', new=self.mock_return_value) as mock_read:
+            with patch.object(self.processor, 'main_position_player_file', new=mock_file_pos)\
+                    as mock_get_file_pos:
+                with patch.object(self.processor, 'position_dictionary', new=mock_pos)\
+                        as mock_get_long_pos:
+                    with patch.object(self.processor, 'shortened_dictionary', new=mock_short_pos)\
+                            as mock_get_short_pos:
+                        result = self.processor.set_player_data(params)
+                        expected = {"player": "name", "main_pos": "pos_f", "player_pos": "pos_l",
+                                    "main_pos_short": "pos_s", "player_data": "mock"}
+                        mock_read.assert_called_once_with("name")
+                        mock_get_file_pos.assert_called_once_with("mock")
+                        mock_get_long_pos.assert_called_once()
+                        mock_get_short_pos.assert_called_once()
+                        self.assertEqual(expected, result)
+
+    def test_set_tactalyse_data(self):
+        params = {"start_date": "start", "end_date": "end"}
+        result = self.processor.set_tactalyse_data(params, self.line_map)
+        params["key"] = "value"
+        expected = params
+        self.assertEqual(expected, result)
+
+    def test_set_tactalyse_data_no_data(self):
+        params = {}
+        result = self.processor.set_tactalyse_data(params, self.line_map)
+        params = {"start_date": None, "end_date": None}
+        params["key"] = "value"
+        expected = params
+        self.assertEqual(expected, result)
+
+    def test_set_stats(self):
+        params = {'stat': 'stat'}
+        result = self.processor.set_stats(params, self.line_map)
+        expected = {'key': 'value', 'columns': ['stat']}
+        self.assertEqual(expected, result)
+
+    def test_set_stats_no_stat(self):
+        params = {}
+        with patch.object(self.processor.randomizer, 'random_player_stat', new=self.mock_return_value):
+            result = self.processor.set_stats(params, self.line_map)
+            expected = {'key': 'value', 'columns': ['mock']}
+            self.assertEqual(expected, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
